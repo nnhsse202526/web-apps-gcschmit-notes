bf6c3d10250766f7e4cb2421efdded596f207d8d
Matt Callaghan <mcallaghan@naperville203.org>
2025-11-20 09:18:24 -0600

    added css and client-side js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,18 @@
 // client-side JavaScript to handle events in the browser
+
+// the querySelector function returns the first element in the document
+// that matches the specified CSS selector (e.g., h1)
+
+const myHeading = document.querySelector("h1");
+
+// javascript can modify an attribute of an HTML element
+myHeading.textContent = "Hello world!";
+
+// the querySelector method can also be used to select elements by class name
+const button = document.querySelector("button.demo");
+
+// the addEventListener function attaches an event handler (i.e., function)
+// to the specified element (i.e., button) for the specified event (i.e., "click")
+button.addEventListener("click", () => {
+  alert("Ouch! Stop poking me!");
+});
