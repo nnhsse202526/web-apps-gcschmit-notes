0323f857205d572c176d0533751a2318ca2da067
Geoff Schmit <gcschmit@gmail.com>
2025-11-24 09:34:48 -0600

    Start notes on MongoDB.

--- db.js
+++ db.js
@@ -3,3 +3,21 @@
 /**********************************************************
  * MongoDB
  **********************************************************/
+
+const { MongoClient } = require("mongodb");
+
+// load environment variables for .env file into process.env
+const dotenv = require("dotenv");
+dotenv.config({ path: ".env" });
+
+const client = new MongoClient(process.env.MONGO_URI);
+
+async function run() {
+  try {
+    await client.connect();
+
+    // obtain references to a database and one of its collections
+    const db = 
+
+  }
+}
